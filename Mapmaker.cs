3d1671d483de8e98dc918defdb543aab231dad1a
Petr Bernatskii <pebrnt@gmail.com>
2024-01-19 17:54:43 +0500

    [WIP] A set of edits no 16
    - Added Mapmaker class (experimental and not tested)
    - Configuration: Mapmaker now can be Standard, X1, X2:
      - Standard - original data fetcher and MapImageGeneration class as renderer
      - X1 - tweaked data fetcher and tweaked MapImageGeneration class as renderer
      - X2 - tweaked data fetcher and Mapmaker class as renderer
    - Added forest mask save/load functionality
    - Added logging of elapsed time

---
+++ Mapmaker.cs
@@ -0,0 +1,454 @@
+﻿using System.Collections;
+using System.Threading;
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+public class Mapmaker
+{
+    //private static readonly Color32 ChartMapColor = new Color32(203, 155,  87, byte.MaxValue);
+    private static readonly Color32 OceanColor    = new Color32( 20, 100, 255, byte.MaxValue);
+    private static readonly Color32 FogColor      = new Color32(203, 155,  87, byte.MaxValue);
+
+    //private int                _mapPixelCount;
+    private readonly int       _mapSize;
+
+    //private Color32[] _scenery;       // -> World
+    //private Color32[] _contours;
+    //private Color32[] _fog;
+    //private Color32[] _fogTexture;    //
+    //private Color32[] _abyss;
+    //private Color32[] _abyssTexture;  //
+
+    private Color32[] _biomes;
+    private Color32[] _heights;
+    private Color32[] _forest;
+    private Color32[] _explored;
+
+    private readonly int       _contourInterval;
+
+    //public Color32[] World;        // entire world map
+    public Color32[] WorldMap;     // entire world map with contours
+    public Color32[] ExploredMap;  // explored map with contours
+    //public Color32[] WorldMask;
+
+    public Color32   AbyssColor { get; set; } = new Color32(0, 0, 0, byte.MaxValue);
+
+
+    public Mapmaker(int mapSize, Color32[] biomes, Color32[] heights, Color32[] forest, Color32[] explored, int contourInterval)
+    {
+        _mapSize         = mapSize;
+        _biomes          = biomes;
+        _heights         = heights;
+        _forest          = forest;
+        _explored        = explored;
+        _contourInterval = contourInterval;
+
+        //_mapPixelCount   = _mapSize * _mapSize;
+    }
+
+
+    ~Mapmaker()
+    {
+        _biomes   = null;
+        _heights  = null;
+        _forest   = null;
+        _explored = null;
+    }
+
+
+    public Color32[] RenderTopographicalMap()
+    {
+        Color32[] mask           = _biomes;     // TODO: use selector
+        Color32   maskClearColor = AbyssColor;
+
+        if (WorldMap == null)
+        {
+            Color32[] canvas = ReplaceColor(null, _biomes, AbyssColor, Color.white);
+
+            canvas = RenderWater(canvas, _heights, mask, maskClearColor, _mapSize, 4, 64);
+
+            canvas = DarkenLinear(canvas, canvas, 20, mask, maskClearColor);
+
+            canvas = DarkenRelative(canvas, canvas, 0.85f, _forest, Color.clear);
+
+            canvas = RenderContours(canvas, _heights, _contourInterval, 128, mask, maskClearColor, _mapSize);
+
+            WorldMap = canvas;
+        }
+
+        ExploredMap = RenderFog(null, WorldMap, _explored, mask, maskClearColor, _mapSize, 128, 16);
+
+        return ExploredMap;
+    }
+
+
+    public static IEnumerable RunAsCoroutine(Action action)
+    {
+        var thread = new Thread(() => action());
+
+        thread.Start();
+        while (thread.IsAlive)
+        {
+            yield return null;
+        }
+    }
+
+
+    private static Color32[] RenderContours(Color32[] canvas, Color32[] heights, int interval, byte alpha, Color32[] mask, Color32 maskClearColor, int size)
+    {
+        bool noBlending  = canvas == null;
+        byte halfAlpha   = (byte)(alpha / 2);
+        byte quoterAlpha = (byte)(alpha / 4);
+
+        canvas = canvas ?? new Color32[heights.Length];
+
+        Color32[] maskProxy = mask ?? new Color32[1];
+        int       maskInc   = mask != null ? 1 : 0;
+
+        if (mask == null)
+        {
+            maskClearColor = Color.clear;
+            maskProxy[0]   = Color.white;
+        }
+
+        int[] contourHeights = Array.ConvertAll(heights, x => x.rgba <= 0 ? 0 : (int)((x.rgba + interval) / interval));
+
+        unsafe
+        {
+            fixed (Color32 *maskBegin = maskProxy)
+            {
+                fixed (Color32 *canvasBegin = canvas)
+                {
+                    fixed (int *heightsBegin = contourHeights)
+                    {
+                        // x + +
+                        // + o +
+                        // + + +
+
+                        int     *height     = heightsBegin;
+                        int     *height1    = height       + size;
+                        int     *height2    = height1      + size;
+                        int     *heightsEnd = heightsBegin + contourHeights.Length - 2*size - 2;
+                        Color32 *pixel      = canvasBegin  + size + 1;
+                        Color32 *m          = maskBegin    + size + 1;
+                        int      x          = 0;
+                        int      xEnd       = size - 2;
+
+                        while (height < heightsEnd)
+                        {
+                            if (mask == null || m->rgba != maskClearColor.rgba)
+                            {
+                                int hNW = *(height);
+                                int hN  = *(height  + 1);
+                                int hNE = *(height  + 2);
+                                int hW  = *(height1);
+                                int h   = *(height1 + 1);
+                                int hE  = *(height1 + 2);
+                                int hSW = *(height2);
+                                int hS  = *(height2 + 1);
+                                int hSE = *(height2 + 2);
+
+                                if (hN < h || hW < h || hE < h || hS < h)
+                                {
+                                    // detected at orthogonal cell => set full alpha
+                                    byte a = (h % 5) == 1 ? alpha : halfAlpha;  // Keep full alpha for every 5th graduation line. Half alpha for the rest
+
+                                    if (noBlending)
+                                        pixel->a = a;
+                                }
+                                else if (hNW < h || hNE < h || hSW < h || hSE < h)
+                                {
+                                    // detected at diagonal cell => set half alpha for a smoother effect if not full alpha already
+                                    byte a = (h % 5) == 1 ? halfAlpha : quoterAlpha;  // Keep full alpha for every 5th graduation line. Half alpha for the rest
+
+                                    if (noBlending)
+                                        pixel->a = a;
+                                }
+                            }
+
+                            ++height;
+                            ++height1;
+                            ++height2;
+                            ++pixel;
+                            m += maskInc;
+
+                            if (++x < xEnd)
+                                continue;
+
+                            height  += 2;
+                            height1 += 2;
+                            height2 += 2;
+                            pixel   += 2;
+                            m += maskInc + maskInc;
+
+                            x = 0;
+                        }  // while
+                    }  // fixed
+                }  // fixed
+            }  // fixed
+        }  // unsafe
+
+        return canvas;
+    }
+
+
+    private static Color32[] RenderWater(Color32[] canvas, Color32[] heights, Color32[] mask, Color32 maskClearColor, int size, int tightness, int damping)
+    {
+        bool    noBlending = canvas == null;
+        Color32 c          = new Color32();
+
+        canvas = canvas ?? new Color32[heights.Length];
+
+        for (int i = 0; i < heights.Length; ++i)
+        {
+            if (mask != null && mask[i].rgba == maskClearColor.rgba)
+                continue;
+
+            if (heights[i].rgba > 0)
+            {
+                // above sea level
+                if (noBlending)
+                    canvas[i] = Color.clear;
+            }
+            else
+            {
+                int a = -heights[i].rgba * 16 + 128;
+
+                ref Color32 pixel = ref canvas[i];
+
+                if (tightness > 0 && damping > 0)
+                {
+                    float noise = Mathf.PerlinNoise((float)(i / size) / tightness, (float)(i % size) / tightness) - 0.5f;
+                    byte  delta = (byte)(255 * noise / damping);
+                    c.r = (byte)(OceanColor.r + delta);
+                    c.g = (byte)(OceanColor.g + delta);
+                    c.b = (byte)(OceanColor.b + delta);
+                }
+                else
+                {
+                    c.rgba = OceanColor.rgba;
+                }
+
+                c.a = (byte)(a > 255 ? 255 : a);
+
+                if (noBlending)
+                {
+                    pixel.rgba = c.rgba;
+                }
+                else
+                {
+                    BlendColor(ref pixel, pixel, c);
+                }
+            }
+        }
+
+        return canvas;
+    }
+
+
+    private static Color32[] RenderFog(Color32[] canvas, Color32[] layer, Color32[] explored, Color32[] mask, Color32 maskClearColor, int size, int tightness, int damping)
+    {
+        canvas = canvas ?? new Color32[explored.Length];
+
+        for (int i = 0; i < explored.Length; ++i)
+        {
+            if (mask != null && mask[i].rgba == maskClearColor.rgba)
+            {
+                canvas[i] = layer[i];
+                continue;
+            }
+
+            if (explored[i].a > 0)
+            {
+                canvas[i] = layer[i];
+                continue;
+            }
+
+            ref Color32 pixel = ref canvas[i];
+
+            if (tightness > 0 && damping > 0)
+            {
+                float noise = Mathf.PerlinNoise((float)(i / size) / tightness, (float)(i % size) / tightness) - 0.5f;
+                byte  delta = (byte)(255 * noise / damping);
+                pixel.r = (byte)(FogColor.r + delta);
+                pixel.g = (byte)(FogColor.g + delta);
+                pixel.b = (byte)(FogColor.b + delta);
+                pixel.a = (byte)(255);
+            }
+            else
+            {
+                pixel.rgba = FogColor.rgba;
+            }
+        }
+
+        return canvas;
+    }
+
+
+    private static Color32[] DarkenLinear(Color32[] canvas, Color32[] layer, byte d, Color32[] mask, Color32 maskClearColor)
+    {
+        canvas = canvas ?? new Color32[layer.Length];
+
+        for (int i = 0; i < layer.Length; ++i)
+        {
+            if (mask != null && mask[i].rgba == maskClearColor.rgba)
+            {
+                canvas[i] = layer[i];
+                continue;
+            }
+
+            ref Color32 pixel = ref canvas[i];
+            Color32     c     = layer[i];
+
+            pixel.rgba = 0;
+            pixel.a    = c.a;
+
+            int value = c.r - d;
+            if (value > 0)
+                pixel.r = (byte)value;
+
+            value = c.g - d;
+            if (value > 0)
+                pixel.g = (byte)value;
+
+            value = c.b - d;
+            if (value > 0)
+                pixel.b = (byte)value;
+        }
+
+        return canvas;
+    }
+
+
+    private static Color32[] DarkenRelative(Color32[] canvas, Color32[] layer, float dimFactor, Color32[] mask, Color32 maskClearColor)
+    {
+        canvas = canvas ?? new Color32[layer.Length];
+
+        for (int i = 0; i < layer.Length; ++i)
+        {
+            if (mask != null && mask[i].rgba == maskClearColor.rgba)
+            {
+                canvas[i] = layer[i];
+                continue;
+            }
+
+            ref Color32 target = ref canvas[i];
+            ref Color32 source = ref layer[i];
+
+            target.r = (byte)(source.r * dimFactor);
+            target.g = (byte)(source.g * dimFactor);
+            target.b = (byte)(source.b * dimFactor);
+            target.a = source.a;
+        }
+
+        return canvas;
+    }
+
+
+    private static Color32[] ReplaceColor(Color32[] canvas, Color32[] layer, Color32 from, Color32 to)
+    {
+        canvas = canvas ?? new Color32[layer.Length];
+
+        for (int i = 0; i < layer.Length; ++i)
+        {
+            Color32 ip = layer[i];
+
+            if (ip.rgba == from.rgba)
+                canvas[i] = to;
+            else
+                canvas[i] = ip;
+        }
+
+        return canvas;
+    }
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static byte BlendColorChannel(byte c1, byte alpha1, byte c2, byte alpha2, byte alpha)
+    {
+        //return alpha1 == 0 || alpha2 == 255 ? c2 : (byte)(c2 + (c1 - c2) * (255 - alpha2) * alpha1 / 65025);
+        return alpha1 == 0 || alpha2 == 255
+            ? (byte)(c2 * alpha2 / alpha)
+            : (byte)(c2 * alpha2 / alpha + c1 * alpha1 * (255 - alpha2) / 255 / alpha);
+    }
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static byte BlendAlphaChannel(byte alpha1, byte alpha2)
+    {
+        return alpha1 == 255 || alpha2 == 0
+            ?  alpha1
+            : (
+                alpha2 == 255
+                    ? (byte)255
+                    : (byte)(alpha1 + ((255 - alpha1) * alpha2) / 255)
+            );
+    }
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void BlendColor(ref Color32 c, Color32 c1, Color32 c2)
+    {
+        c.a = BlendAlphaChannel(c1.a, c2.a);
+
+        c.r = BlendColorChannel(c1.r, c1.a, c2.r, c2.a, c.a);
+        c.g = BlendColorChannel(c1.g, c1.a, c2.g, c2.a, c.a);
+        c.b = BlendColorChannel(c1.b, c1.a, c2.b, c2.a, c.a);
+    }
+
+
+    private static Color32[] Blend(Color32[] canvas, Color32[] layer, float factor, bool[] mask)
+    {
+        //Color32 c = new Color32();
+        canvas = canvas ?? new Color32[layer.Length];
+
+        for (int i = 0; i < canvas.Length; ++i)
+        {
+            if (mask != null && !mask[i])
+                continue;
+
+            Color32 c1 = canvas[i];
+            Color32 c2 = layer[i];
+
+            //float a1 = ((Color)canvas[i]).a;
+            //float a2 = ((Color)array2[i]).a;
+
+            //c.a = BlendAlphaChannel(c1.a, c2.a);
+            //c.r = BlendColorChannel(c1.r, c1.a, c2.r, c2.a, c.a);
+            //c.g = BlendColorChannel(c1.g, c1.a, c2.g, c2.a, c.a);
+            //c.b = BlendColorChannel(c1.b, c1.a, c2.b, c2.a, c.a);
+
+            BlendColor(ref canvas[i], c1, c2);
+        }
+
+        return canvas;
+    }
+
+    // refined copy of IEnumerator OverlayTexture(Color32[] array1, Color32[] array2, bool allMap = false)
+    private static Color32[] BlendByLerp(Color32[] canvas, Color32[] layer, bool[] mask)
+    {
+        canvas = canvas ?? new Color32[layer.Length];
+
+        for (int i = 0; i < layer.Length; ++i)
+        {
+            if (mask != null && !mask[i])
+                continue;
+
+            Color c1 = canvas[i];
+            Color c2 = layer[i];
+
+            Color c  = Color.Lerp(((Color)c1), ((Color)c2), c2.a);
+
+            //c.a = a2 + a1;
+            //if (c.a > 1)
+            //    c.a = 1;
+            c.a = c1.a + (255 - c1.a) * c2.a / 255;
+
+            canvas[i] = c;
+        }
+
+        return canvas;
+    }
+}
